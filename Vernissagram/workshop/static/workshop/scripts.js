9ca1f5e84dd4c75e50592d9bb96f267d19743b42
Milosz <miloszw@kth.se>
2013-06-22 11:55:48 +0200

    Adds ok-button and other effects to workshop.

--- Vernissagram/workshop/static/workshop/scripts.js
+++ Vernissagram/workshop/static/workshop/scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
+    $('.thumbnails img').hover(function() {
+        $(this).removeClass('grayscale');
+    }, function() {
+        if(!$(this).hasClass('highlighted'))
+            $(this).addClass('grayscale');
+    });
+    
     $('#tab1 li img').click(function(e) {
         e.preventDefault();
+        if($(this).hasClass('highlighted'))
+            return;
+        $('.thumbnails .highlighted').addClass('grayscale');
         $('.thumbnails .highlighted').removeClass('highlighted');
         $(this).addClass('highlighted');
         $src = $(this).attr('src');
         $('#img-large').attr('src',$src);
+    });
+    
+    $('#lbox #ok-btn').hide();
+    $('#lbox').hover(function() {
+        $('#lbox #ok-btn').stop().fadeIn('fast');
+    }, function() {
+        $('#lbox #ok-btn').stop().fadeOut('fast');
     });
 });
 
